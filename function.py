1982e9706bc7ab81c33a7e94b8a204eabe5acaab
Sahilsadik1 <51982536+Sahilsadik1@users.noreply.github.com>
2019-06-20 14:13:47 +0530

    Add files via upload

---
+++ function.py
@@ -0,0 +1,9 @@
+def user_input():
+        servername =input("Enter name of the server:")
+        dbname= input("Enter name of the database:")
+        username=input("Enter your username:")
+        password=input("enter your password:")
+        a="servername="+servername+";"+"dbname="+dbname+";"+"username="+username+";"+"password="+password
+        return a
+data=user_input()
+print(data)
