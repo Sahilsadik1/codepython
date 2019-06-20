49cd3d2f9d556b9b12e8d3f6146e575c65aaa2da
Sahilsadik1 <51982536+Sahilsadik1@users.noreply.github.com>
2019-06-20 15:40:02 +0530

    Update function.py

--- function.py
+++ function.py
@@ -9,3 +9,17 @@ data=user_input()
 print(data)
                        
         
+OR 
+def user_input():
+        servername =input("Enter name of the server:")
+        dbname= input("Enter name of the database:")
+        username=input("Enter your username:")
+        password=input("enter your password:")
+                    
+        return servername,dbname,username,password
+def main():
+               servername,dbname,username,password=user_input()
+               s,d,u,p=(servername,dbname,username,password)
+               print("The string names are:",s,d,u,p)
+               return s,d,u,p
+main()             
